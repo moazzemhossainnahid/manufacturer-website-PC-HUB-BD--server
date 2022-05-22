3530ca2d9384e374c51d567164aef14241e525f7
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-05-22 15:30:08 +0600

    specific product find

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -16,14 +16,34 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hmmg8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("DB Connected");
-  client.close();
-});
 
+const run = async() => {
+    try{
+        await client.connect();
+        const productsCollection = client.db("PCHubBD").collection("Products");
 
+        // get products
+        app.get('/products', async(req, res) => {
+            const query = {};
+            const products = productsCollection.find(query);
+            const result = await products.toArray();
+            res.send(result);
+        })
+
+        // get product
+        app.get('/product/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
+
+    }finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("Running PC Hub BD Server");
